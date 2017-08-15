bd03859088b435684fb45a249d08862fae4a34bb
Huy Roberto <huyroberto@gmail.com>
2017-08-15 16:16:37 +0700

    Init 2

---
+++ Rapid.Web/Controllers/ManifestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rapid.Web.Controllers
+{
+    public class ManifestController : Controller
+    {
+        // GET: Manifest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Confirm()
+        {
+            return View();
+        }
+    }
+}
